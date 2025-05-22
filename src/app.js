a9e50fe7567a4767be755841ae468f1cf2d463d1
Espla <37451532+alejandroespla@users.noreply.github.com>
2025-05-22 09:00:46 +0000

    new

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let domainElement = document.querySelector('#domain'); 
+
+  let pronoun = ['the', 'our', 'a', 'his', 'notyours', 'takea'];
+  let adj = ['great', 'big', 'small', 'huge', 'ridiculous', ];
+  let noun = ['jogger', 'racoon', 'car', 'mother', 'friend', 'house'];
+  let extension = ['.com', '.es', '.net', '.org', '.edu', '.us']
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);  
+  let adjIndex = Math.floor(Math.random() * adj.length);  
+  let nounIndex = Math.floor(Math.random() * noun.length);  
+  let extensionIndex = Math.floor(Math.random() * extension.length);  
+  
+  let domain = `${pronoun[pronounIndex]} ${adj[adjIndex]} ${noun[nounIndex]} ${extension[extensionIndex]}`;  
+  domainElement.textContent = domain;
 };
